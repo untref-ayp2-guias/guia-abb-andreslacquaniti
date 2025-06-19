7f7c51a2e0e057020f8f3d66d0fa51fbdb2ba2bd
Andrés Lacquanitti <andres.lacquanitti@boldt.com.ar>
2025-06-19 19:20:05 -0300

    Todo OK

--- ejercicios/ej02.go
+++ ejercicios/ej02.go
@@ -1,11 +1,44 @@
 package ejercicios
 
-import (
-	"untref-ayp2/guia-abb/binarytree"
-)
+type Tarea struct {
+	Nombre string
+	Tiempo int
+}
 
-func PredecesorInOrder(bt *binarytree.BinarySearchTree[int], k int) (int, error) {
-	// Implementar
-	var x int
-	return x, nil
+func Ejercicio2(tareas []Tarea) {
+	tareasOrdenadas := ordenar(tareas)
+	for i := range tareas {
+		tareas[i] = tareasOrdenadas[i]
+	}
+}
+
+func ordenar(tareas []Tarea) []Tarea {
+	if len(tareas) == 0 {
+		return make([]Tarea, 0)
+	}
+	corta, pos := buscarMasCorta(tareas)
+	restantes := rearmar(tareas, pos)
+	return append([]Tarea{corta}, ordenar(restantes)...)
+}
+
+func rearmar(tareas []Tarea, pos int) []Tarea {
+	slice1 := tareas[:pos]
+	slice2 := tareas[pos+1:]
+	slice1 = append(slice1, slice2...)
+	return slice1
+}
+func buscarMasCorta(tareas []Tarea) (Tarea, int) {
+	var masCorta Tarea
+	if len(tareas) == 0 {
+		return masCorta, 0
+	}
+	masCorta = tareas[0]
+	indice := 0
+	for i, tarea := range tareas {
+		if tarea.Tiempo < masCorta.Tiempo {
+			masCorta = tarea
+			indice = i
+		}
+	}
+	return masCorta, indice
 }
--- ejercicios/ej02_test.go
+++ ejercicios/ej02_test.go
@@ -3,58 +3,25 @@ package ejercicios
 import (
 	"testing"
 
-	"untref-ayp2/guia-abb/binarytree"
-
 	"github.com/stretchr/testify/assert"
 )
 
-/* Construct the following tree
-          15
-        /    \
-       /      \
-      10       20
-     / \      /  \
-    /   \    /    \
-   8    12  16    25
-*/
-
-func TestPredecesorBst(t *testing.T) {
-	bst := binarytree.NewBinarySearchTree[int]()
-	bst.Insert(15)
-	bst.Insert(10)
-	bst.Insert(20)
-	bst.Insert(8)
-	bst.Insert(12)
-	bst.Insert(16)
-	bst.Insert(25)
-
-	_, err := PredecesorInOrder(bst, 8)
-	assert.EqualError(t, err, "No hay predecesores menores que el mínimo")
-	p, _ := PredecesorInOrder(bst, 10)
-	assert.Equal(t, 8, p)
-	p, _ = PredecesorInOrder(bst, 12)
-	assert.Equal(t, 10, p)
-	p, _ = PredecesorInOrder(bst, 15)
-	assert.Equal(t, 12, p)
-	p, _ = PredecesorInOrder(bst, 16)
-	assert.Equal(t, 15, p)
-	p, _ = PredecesorInOrder(bst, 20)
-	assert.Equal(t, 16, p)
-	p, _ = PredecesorInOrder(bst, 25)
-	assert.Equal(t, 20, p)
-}
-
-func TestNilPredecesorBst(t *testing.T) {
-	bst := binarytree.NewBinarySearchTree[int]()
-
-	_, err := PredecesorInOrder(bst, 16)
-	assert.EqualError(t, err, "No hay predecesores")
-}
-
-func TestUnoSoloPredecesorBst(t *testing.T) {
-	bst := binarytree.NewBinarySearchTree[int]()
-	bst.Insert(100)
-
-	_, err := PredecesorInOrder(bst, 16)
-	assert.EqualError(t, err, "No hay predecesores menores que el mínimo")
+func TestEjercicio2(t *testing.T) {
+	tareas := []Tarea{
+		{"t1", 34},
+		{"t2", 13},
+		{"t3", 16},
+		{"t4", 7},
+		{"t5", 9},
+		{"t6", 19},
+		{"t7", 10},
+		{"t8", 21},
+		{"t9", 42},
+		{"t10", 14},
+		{"t11", 25},
+	}
+	Ejercicio2(tareas)
+	for i := 0; i < len(tareas)-1; i++ {
+		assert.LessOrEqualf(t, tareas[i].Tiempo, tareas[i+1].Tiempo, "Las tareas no están ordenadas en la posición %d", i)
+	}
 }
